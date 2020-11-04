1ab981c8aa0785942d339e824f5a8d9f7df7214d
Antonio Dias <addsjunior@gmail.com>
2020-11-04 09:46:05 -0300

    rotas de cliente

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const models = require('./models');
 const Produto = models.Produto;
 const Fabricante = models.Fabricante;
+const Cliente = models.Cliente;
 app.use(logging);
 app.use(bodyParser.json());
 
@@ -40,6 +41,44 @@ app.delete('/produtos/:id', (req, res) => {
       res.json(err);
     })
 });
+
+app.delete('/clientes/:id', (req, res) => {
+  const id = parseInt(req.params.id, 10);
+
+  Cliente.destroy({
+    where: { id }
+  }).then((linhas) => {
+    if (linhas > 0) {
+      res.json({ msg: "O cliente foi removido." });
+    } else {
+      res.json({ msg: "O cliente não existe." });
+    }
+  }).catch((err) => {
+      res.json(err);
+    })
+});
+
+app.put('/clientes', (req, res) => {
+  const { id, nome, sobrenome } = req.body
+  const parametros = {}
+
+  if (nome) parametros.nome = nome
+  if (sobrenome) parametros.sobrenome = sobrenome
+
+  Cliente.update(parametros, {
+    where: { id: id }
+  })
+  .then((linhas) => {
+    if (linhas[0] > 0) {
+      res.json({ msg: "O cliente foi atualizado." });
+    } else {
+      res.json({ msg: "O cliente não existe." });
+    }
+  })
+  .catch((err) => {
+    res.json(err);
+  })
+})
 
 app.put('/produtos', (req, res) => {
   const { nome, codigo, preco, fabricanteId } = req.body;
@@ -84,6 +123,46 @@ app.get('/fabricantes', (req, res) => {
       res.json(err);
     })
 });
+
+// Rotas - cliente
+// POST /clientes (criar)
+// GET /clientes (recuperar todos)
+// GET /clientes/:id (recuperar cliente :id)
+// PUT /clientes (atualizar)
+// DELETE /clientes/:id (remover)
+
+app.post('/clientes', (req, res) => {
+  if (req.body.hasOwnProperty('nome') &&
+    req.body.hasOwnProperty('sobrenome')) {
+    
+    const { nome, sobrenome } = req.body;
+    // em vez de
+    // const nome = req.body.nome;
+    // const sobrenome = req.body.sobrenome;
+
+    Cliente.create({ nome, sobrenome })
+    .then((cliente) => {
+      res.json(cliente)
+    })
+    .catch(function(erro) {
+      res.json(erro)
+    })
+  } else {
+    res.status(422).json({ 
+      mensagem: "É necessário especificar nome e sobrenome para o cadastro." 
+    })
+  }
+})
+
+app.get('/clientes', (req, res) => {
+  Cliente.findAll({
+    attributes: [ 'nome', 'sobrenome' ]
+  }).then((clientes) => {
+    res.json(clientes);
+  }).catch((err) => {
+    res.json(err);
+  })
+})
 
 app.post('/fabricantes', (req, res) => {
   if (req.body.hasOwnProperty('nome')) {
